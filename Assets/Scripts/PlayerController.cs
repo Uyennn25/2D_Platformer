0e1f1168fdb7703f5e7b23ce2a2e13a2721c2f5c
Uyennn25 <tranhunguyen4@gmail.com>
2024-09-20 17:32:19 +0700

    movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+   private float movementInputDirection;
+
+   private bool isFacingRight = true; //hướng mặc định của player đang hướng về bên phải.
+   private Rigidbody2D rb;
+
+   public float movementSpeed = 10.0f;
+   private void Start()
+   {
+      rb = GetComponent<Rigidbody2D>();
+   }
+
+   private void Update()
+   {
+      CheckInput();
+      CheckMomentDirection();
+   }
+
+   private void FixedUpdate()
+   {
+      ApplyMovement();
+    
+   }
+
+   private void CheckMomentDirection()// kiểm tra và chuyển hướng cho player cùng hướng với hướng chuyển động đầu vào.
+   {
+      if (isFacingRight && movementInputDirection < 0 )
+      {
+         Flip();
+      }
+
+      if (!isFacingRight && movementInputDirection > 0)
+      {
+         Flip();
+      }
+   }
+   private void CheckInput()
+   {
+      movementInputDirection = Input.GetAxisRaw("Horizontal");
+   }
+
+   private void ApplyMovement()
+   {
+      rb.velocity = new Vector2(movementSpeed * movementInputDirection, rb.velocity.y);
+   }
+
+   private void Flip() // hàm lật player.
+   {
+      isFacingRight = !isFacingRight;
+      transform.Rotate(0.0f, 180.0f,0.0f);
+      
+   }
+}
