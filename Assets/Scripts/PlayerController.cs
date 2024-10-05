7943c6a6ac07863320d206a6597db07a3ae2b2aa
Uyennn25 <tranhunguyen4@gmail.com>
2024-10-05 23:31:54 +0700

    WallSlide

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,8 @@ public class PlayerController : MonoBehaviour
    private bool isFacingRight = true; //hướng mặc định của player đang hướng về bên phải.
    private bool isWalking;
    private bool isGround;
+   private bool isTouchingWall;
+   private bool isWallSliding;
    private bool canJump;
   
    private Rigidbody2D rb;
@@ -16,11 +18,14 @@ public class PlayerController : MonoBehaviour
    public float movementSpeed = 10.0f;
    public float jumpForce = 16.0f;
    public float groundCheckRadius;
+   public float wallCheckDistance;
+   public float wallSlideSpeed;
 
    public int amountOfJump = 1;
    
 
    public Transform groundCheck;
+   public Transform wallCheck;
 
    public LayerMask whatIsGround;
    private void Start()
@@ -36,6 +41,7 @@ public class PlayerController : MonoBehaviour
       CheckMomentDirection();
       AppdateAnimations();
       CheckIfCanJump();
+      CheckIfWallSliding();
    }
 
    private void FixedUpdate()
@@ -45,6 +51,17 @@ public class PlayerController : MonoBehaviour
     
    }
 
+   private void CheckIfWallSliding()
+   {
+      if (isTouchingWall && !isGround && rb.velocity.y < 0)
+      {
+         isWallSliding = true;
+      }
+      else
+      {
+         isWallSliding = false;
+      }
+   }
    private void CheckIfCanJump()
    {
       if (isGround && rb.velocity.y <= 0)
@@ -64,9 +81,10 @@ public class PlayerController : MonoBehaviour
    private void CheckSurroundings()
    {
       isGround = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+      isTouchingWall = Physics2D.Raycast(wallCheck.position, transform.right, wallCheckDistance, whatIsGround);
       /*
        Physics2D.OverlapCircle():
-         Đây là một hàm của Unity dùng để kiểm tra xem có bất kỳ collider nào của các đối tượng khác chồng lấn 
+         Đây là một hàm của Unity dùng để kiểm tra xem có bất kỳ collider nào của các đối tượng khác chồng lấn
          với một hình tròn đã cho hay không.
          Các tham số của hàm:
          groundCheck.position: Vị trí tâm của hình tròn kiểm tra.
@@ -103,7 +121,8 @@ public class PlayerController : MonoBehaviour
    {
       anim.SetBool("isWalking", isWalking);
       anim.SetBool("isGround", isGround);
-     anim.SetFloat("yVelocity", rb.velocity.y);
+      anim.SetFloat("yVelocity", rb.velocity.y);
+      anim.SetBool("isWallSliding", isWallSliding);
    }
    private void CheckInput()
    {
@@ -127,19 +146,34 @@ public class PlayerController : MonoBehaviour
    }
    private void ApplyMovement()
    {
-      rb.velocity = new Vector2(movementSpeed * movementInputDirection, rb.velocity.y);
+      if (isGround)
+      {
+         rb.velocity = new Vector2(movementSpeed * movementInputDirection, rb.velocity.y);
+      }
+      if (isWallSliding)
+      {
+         if (rb.velocity.y < -wallSlideSpeed)
+         {
+            rb.velocity = new Vector2(rb.velocity.x, wallSlideSpeed);
+         }
+      }
    }
 
    private void Flip() // hàm lật player.
    {
-      isFacingRight = !isFacingRight;
-      transform.Rotate(0.0f, 180.0f,0.0f);
+      if (!isWallSliding)
+      {
+         isFacingRight = !isFacingRight;
+         transform.Rotate(0.0f, 180.0f,0.0f); 
+      }
+     
       
    }
 
    private void OnDrawGizmos()
    {
       Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+      Gizmos.DrawLine(wallCheck.position, new Vector3(wallCheck.position.x + wallCheckDistance, wallCheck.position.y, wallCheck.position.z));
       /*
         hàm OnDrawGizmos là một hàm có sẵn trong Unity, được cung cấp bởi lớp MonoBehaviour. 
         sử dụng hàm này trong các script của mình để vẽ các gizmos.
@@ -150,6 +184,9 @@ public class PlayerController : MonoBehaviour
          Gizmos.DrawWireSphere: Vẽ một hình cầu chỉ có đường viền.
          Gizmos.DrawWireCube: Vẽ một hình hộp chỉ có đường viền.
          Gizmos.color: Thiết lập màu sắc cho các hình dạng.
+         
+          Gizmos.DrawLine (..):sẽ vẽ một đường thẳng bắt đầu từ vị trí của đối tượng "wallCheck" 
+          và kéo dài theo hướng trục x dương một khoảng cách bằng wallCheckDistance.
        */
    }
 }
